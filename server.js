9b5a0f2cae8e6be6611b8cfe903d69f3b046b5f4
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-04 22:28:15 -0500

    refactor code and make routes and controllers directory

--- server.js
+++ server.js
@@ -1,49 +1,57 @@
+import createError from 'http-errors'
 import express from 'express'
-import { songsArr } from './data/songs-data.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import logger from 'morgan'
 
+// import routers
+import { router as indexRouter } from './routes/index.js'
+import { router as songsRouter } from './routes/songs/index.js'
+
+// set up app
 const app = express()
 
-// Configuure app
-
+// view engine setup
+app.set(
+  'views',
+  path.join(path.dirname(fileURLToPath(import.meta.url)), 'views')
+)
 app.set('view engine', 'ejs')
 
-// Mount Middleware
+// middleware
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(
+  express.static(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
+  )
+)
 
-app.get('/', function(req, res) {
-  res.redirect('/home')
+// mounted routers
+app.use('/', indexRouter)
+app.use('/songs', songsRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
 })
 
-app.get('/home.ejs', function(req, res) {
-  res.redirect('/home')
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
-app.get('/home/home.ejs', function(req, res) {
-  res.redirect('/home')
-})
-
-app.get('/songs.ejs', function(req, res) {
-  res.redirect('/songs')
-})
-
-app.get('/songs/songs.ejs', function(req, res) {
-  res.redirect('/songs')
-})
-
-app.use('/public', express.static('public'))
-
-// Mount Routes
-
-app.get('/home', function(req, res) {
-  res.render('home')
-})
-
-app.get('/songs', function(req, res) {
-  res.render('songs/songs', {
-    songs: songsArr
-  })
-})
-
-// Tell app to listen on port 3000
-app.listen(3000, function() {
+app.listen(3000, ()=> {
   console.log('Listening on port 3000');
 })
+
+export {
+  app
+}
